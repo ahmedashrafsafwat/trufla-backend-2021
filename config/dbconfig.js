b92a8f12810e0c86a6ae45d78d82319d0b99be86
double0a94 <ahmedashrafsafwat@gmail.com>
2021-06-12 23:04:29 +0200

    heroku configs

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,12 +1,30 @@
 const env = process.env;
 console.log(env.DB_HOST)
+
+let host,port,user,password,database
+
+// use server configs or use local configs
+if(env.USE_HEROKU_CONFIG) {
+  host = env.HEROKU_HOST
+  port = env.HEROKU_PORT
+  user = env.HEROKU_USER
+  password =  env.HEROKU_PASSWORD
+  database =  env.HEROKU_DB
+}else {
+
+host = env.DB_HOST
+port = env.DB_PORT
+user = env.DB_USER
+password = env.DB_PASSWORD
+database = env.DB_NAME
+}
 const config = {
   db: { /* don't expose password or any sensitive info, done only for demo */
-    host: env.DB_HOST || 'freedb.tech',
-    port: env.DB_PORT,
-    user: env.DB_USER || 'freedbtech_geshan-lr',
-    password: env.DB_PASSWORD || 'G2VjjQ5d47zyjqX',
-    database: env.DB_NAME || 'freedbtech_language',
+    host  ,
+    port ,
+    user,
+    password ,
+    database ,
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
